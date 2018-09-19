4e6a86694b426a0c38bcd98e11c47fcb8d1cc363
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 10:48:41 -0400

    Cleaned up use_self.rb

--- lib/full_clone/use_self.rb
+++ lib/full_clone/use_self.rb
@@ -1,36 +1,43 @@
 # coding: utf-8
 
+# A mixin for cloning as self.
 module FullCloneSelf
   def full_clone(_progress=nil)
     self
   end
 end
 
+# Numeric supports full clone.
 class Numeric
   include FullCloneSelf
 end
 
+# NilClass supports full clone.
 class NilClass
   include FullCloneSelf
 end
 
+# TrueClass supports full clone.
 class TrueClass
   include FullCloneSelf
 end
 
+# FalseClass supports full clone.
 class FalseClass
   include FullCloneSelf
 end
 
+# Symbols support full clone.
 class Symbol
   include FullCloneSelf
 end
 
+# Regular expressions support full clone.
 class Regexp
   include FullCloneSelf
 end
 
+# Threads support full clone.
 class Thread
   include FullCloneSelf
 end
-
