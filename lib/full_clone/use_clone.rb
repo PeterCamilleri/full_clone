e1debe6e735fc9155da7fbd20af16db7c595c75b
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 10:58:31 -0400

    Cleaned up use_clone.rb

--- lib/full_clone/use_clone.rb
+++ lib/full_clone/use_clone.rb
@@ -1,19 +1,23 @@
 # coding: utf-8
 
+# A mixin for cloning use Ruby clone.
 module FullCloneClone
   def full_clone(_progress=nil)
     clone
   end
 end
 
+# String full clone support.
 class String
   include FullCloneClone
 end
 
+# Enumerator full clone support.
 class Enumerator
   include FullCloneClone
 end
 
+# Matchdata full clone support.
 class MatchData
   include FullCloneClone
 end
