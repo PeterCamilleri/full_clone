972d626c79e34730453b429d910e3e572937408a
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 11:14:59 -0400

    Cleaned up object.rb

--- lib/full_clone/object.rb
+++ lib/full_clone/object.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+# Add full_clone support to all objects.
 class Object
 
   #By default, no instance variables are excluded.
@@ -11,22 +14,32 @@ class Object
     exclude = full_clone_exclude
 
     if exclude.empty?
-      instance_variables.each do |name|
-        value = result.instance_variable_get(name)
-        value = progress[value.object_id] || value.full_clone(progress)
-        result.instance_variable_set(name, value)
-      end
+      result.full_clone_no_exclusions(progress)
     else
-      instance_variables.each do |name|
-        unless exclude.include?(name)
-          value = result.instance_variable_get(name)
-          value = progress[value.object_id] || value.full_clone(progress)
-          result.instance_variable_set(name, value)
-        end
-      end
+      result.full_clone_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_clone with no exclusions
+  def full_clone_no_exclusions(progress)
+    instance_variables.each do |name|
+      value = instance_variable_get(name)
+      value = progress[value.object_id] || value.full_clone(progress)
+      instance_variable_set(name, value)
+    end
+  end
+
+  # Do a full_clone with exclusions
+  def full_clone_with_exclusions(progress, exclude)
+    instance_variables.each do |name|
+      unless exclude.include?(name)
+        value = instance_variable_get(name)
+        value = progress[value.object_id] || value.full_clone(progress)
+        instance_variable_set(name, value)
+      end
+    end
   end
 
 end
