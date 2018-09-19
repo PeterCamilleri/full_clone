5d3c4f7a36e2ad35554df0bd9d82a45572a8aeb9
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 11:22:55 -0400

    Cleaned up array.rb No smells left!

--- lib/full_clone/array.rb
+++ lib/full_clone/array.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+# Add full_clone support to all arrays.
 class Array
 
   #The full_clone method for arrays.
@@ -6,22 +9,32 @@ class Array
     exclude = full_clone_exclude
 
     if exclude.empty?
-      each_index do |name|
-        value = result[name]
-        value = progress[value.object_id] || value.full_clone(progress)
-        result[name] = value
-      end
+      result.full_clone_no_exclusions(progress)
     else
-      each_index do |name|
-        unless exclude.include?(name)
-          value = result[name]
-          value = progress[value.object_id] || value.full_clone(progress)
-          result[name] = value
-        end
-      end
+      result.full_clone_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_clone with no exclusions
+  def full_clone_no_exclusions(progress)
+    each_index do |name|
+      value = self[name]
+      value = progress[value.object_id] || value.full_clone(progress)
+      self[name] = value
+    end
+  end
+
+  # Do a full_clone with exclusions
+  def full_clone_with_exclusions(progress, exclude)
+    each_index do |name|
+      unless exclude.include?(name)
+        value = self[name]
+        value = progress[value.object_id] || value.full_clone(progress)
+        self[name] = value
+      end
+    end
   end
 
 end
