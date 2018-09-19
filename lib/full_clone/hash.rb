1ca7303906260f0a17d9b88d697b472a8a8d31d7
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-19 11:18:32 -0400

    Cleaned up hash.rb

--- lib/full_clone/hash.rb
+++ lib/full_clone/hash.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+# Add full_clone support to all hashes.
 class Hash
 
   #The full_clone method for structs.
@@ -6,22 +9,32 @@ class Hash
     exclude = full_clone_exclude
 
     if exclude.empty?
-      each_key do |name|
-        value = result[name]
-        value = progress[value.object_id] || value.full_clone(progress)
-        result[name] = value
-      end
+      result.full_clone_no_exclusions(progress)
     else
-      each_key do |name|
-        unless exclude.include?(name)
-          value = result[name]
-          value = progress[value.object_id] || value.full_clone(progress)
-          result[name] = value
-        end
-      end
+      result.full_clone_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_clone with no exclusions
+  def full_clone_no_exclusions(progress)
+    each_key do |name|
+      value = self[name]
+      value = progress[value.object_id] || value.full_clone(progress)
+      self[name] = value
+    end
+  end
+
+  # Do a full_clone with exclusions
+  def full_clone_with_exclusions(progress, exclude)
+    each_key do |name|
+      unless exclude.include?(name)
+        value = self[name]
+        value = progress[value.object_id] || value.full_clone(progress)
+        self[name] = value
+      end
+    end
   end
 
 end
